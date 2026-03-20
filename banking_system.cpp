9db5ee001f09510df9075919ec8dcafe79b5b8bd
yuhabarathy <yuhabarathyshanmugalingam@gmail.com>
2026-03-20 14:16:12 +0530

    Created function to create a new account

--- banking_system.cpp
+++ banking_system.cpp
@@ -1,1 +1,33 @@
- 
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <limits>
+using namespace std;
+
+string name;
+double balance;
+long long account_number;
+int pin;
+
+void create_account()
+{
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Enter your name: ";
+    getline(cin, name);
+    if (name == ""){
+        return;
+    }
+    else{
+         srand(time(0));
+        account_number = rand() % 900000000000LL + 100000000000LL;
+        pin = rand()%10000 + 99999;
+        cout << "\nACCOUNT CREATED SUCCESSFULLY!\n";
+        cout <<"\nUsername: "<<name;
+        cout << "\nAccount number: "<< account_number;
+        cout <<"\nPIN number: "<<pin;
+        cout << "\nCurrent balance: $"<< balance <<endl;
+        cout << "===================================";
+    }
+
+}
+
